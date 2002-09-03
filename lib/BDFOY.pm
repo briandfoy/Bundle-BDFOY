c77bfc599c093539a4f45730c455114823791fcb
brian d foy <brian.d.foy@gmail.com>
2002-09-03 10:11:53 +0000

    Initial revision
    git-svn-id: http://brian-d-foy.svn.sourceforge.net/svnroot/brian-d-foy/trunk@296 d9f60a02-fc28-0410-9752-bc0d6c4f57a6

---
+++ lib/BDFOY.pm
@@ -0,0 +1,72 @@
+package Bundle::BDFOY;
+
+$VERSION = '0.50';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::BDFOY - install all of my modules
+
+=head1 SYNOPSIS
+
+cpan Bundle::BDFOY
+
+=head1 CONTENTS
+
+Apache::Htaccess
+
+Business::ISBN
+
+Chemistry::Elements
+
+ConfigReader::Simple
+
+HTML::SimpleLinkExtor
+
+HTTP::SimpleLinkChecker
+
+HTTP::Size
+
+Mac::iTunes
+
+Mac::PropertyList
+
+Math::NoCarry
+
+Netscape::Bookmarks
+
+Object::Iterate
+
+Set::CrossProduct
+
+Test::Data
+
+Test::HTTPStatus
+
+Test::Manifest
+
+Test::Pod
+
+Tie::Cycle
+
+Tie::Toggle
+
+=head1 AUTHOR
+
+brian d foy, E<lt>bdfoy@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2002, brian d foy, All rights reserved.
+
+You can use this module under the same terms as Perl
+itself.
+
+=cut
+
+
+
+	
