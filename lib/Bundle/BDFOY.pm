787bceadc1f940d00b04abb24f775af65da35b7b
brian d foy <brian.d.foy@gmail.com>
2016-01-26 13:30:18 -0500

    Update the module list

--- lib/Bundle/BDFOY.pm
+++ lib/Bundle/BDFOY.pm
@@ -1,6 +1,6 @@
 package Bundle::BDFOY;
 
-$VERSION = '20150101';
+$VERSION = '20160101';
 
 1;
 
@@ -18,57 +18,33 @@ Bundle::BDFOY - install all modules by BDFOY
 
 =head1 CONTENTS
 
-Acme::BDFOY
-
 App::Module::Lister
 
 App::PPI::Dumper
 
+App::scriptdist
+
 Brick
 
-Bundle::MasteringPerl
-
-Business::ISBM::Data
+Bundle::BDFOY
 
 Business::ISBN
-
-Business::ISBN10
-
-Business::ISBN13
 
 Business::ISBN::Data
 
 Business::ISMN
 
-Business::ISMN::Data
+Business::ISSN
 
-Business::US::USPS::WebTools::AddressStandardization
-
-Business::US::USPS::WebTools::CityStateLookup
-
-Business::US::USPS::WebTools::ZipCodeLookup
+Business::US::USPS::WebTools
 
 CACertOrg::CA
-
-CPAN::Mini::Inject
-
-CPAN::Modulelist
-
-CPAN::PackageDetails
-
-CPAN::PackageDetails::Entries
-
-CPAN::PackageDetails::Entry
-
-CPAN::PackageDetails::Header
-
-CPAN::PackageDetails::PerlHash
-
-CPAN::PackageDetails::Redis
 
 Chemistry::Elements
 
 ConfigReader::Simple
+
+CPAN::PackageDetails
 
 Crypt::Rijndael
 
@@ -78,17 +54,31 @@ Distribution::Cooker
 
 Distribution::Guess::BuildSystem
 
+Dumbbench
+
 File::Find::Closures
+
+File::Fingerprint
 
 Geo::GeoNames
 
 Git::Github::Creator
 
+github_creator
+
+grepurl
+
 HTML::SimpleLinkExtor
 
 HTTP::Cookies::Chrome
 
-HTTP::Cookies::Chrome::Record
+HTTP::Cookies::iCab
+
+HTTP::Cookies::Mozilla
+
+HTTP::Cookies::Omniweb
+
+HTTP::Cookies::Safari
 
 HTTP::SimpleLinkChecker
 
@@ -96,53 +86,19 @@ HTTP::Size
 
 IO::Interactive
 
+Log::Log4perl::Appender::ScreenColoredLevels::UsingMyColors
+
 Mac::Errors
+
+Mac::iPhoto::Shell
+
+Mac::iTerm::LaunchPad
 
 Mac::OSVersion
 
 Mac::Path::Util
 
 Mac::PropertyList
-
-Mac::PropertyList::Boolean
-
-Mac::PropertyList::Container
-
-Mac::PropertyList::Item
-
-Mac::PropertyList::LineListSource
-
-Mac::PropertyList::ReadBinary
-
-Mac::PropertyList::Scalar
-
-Mac::PropertyList::Source
-
-Mac::PropertyList::TextSource
-
-Mac::PropertyList::WriteBinary
-
-Mac::PropertyList::array
-
-Mac::PropertyList::data
-
-Mac::PropertyList::date
-
-Mac::PropertyList::dict
-
-Mac::PropertyList::false
-
-Mac::PropertyList::integer
-
-Mac::PropertyList::real
-
-Mac::PropertyList::string
-
-Mac::PropertyList::true
-
-Mac::PropertyList::ustring
-
-Mac::iTerm::LaunchPad
 
 MacOSX::Alias
 
@@ -154,125 +110,37 @@ Module::Extract::Namespaces
 
 Module::Extract::Use
 
-Module::Extract::Use::Item
-
 Module::Extract::VERSION
 
 Module::Release
 
-Module::Release::CVS
-
-Module::Release::FTP
-
 Module::Release::Git
-
-Module::Release::Kwalitee
-
-Module::Release::MANIFEST
-
-Module::Release::PAUSE
-
-Module::Release::Prereq
-
-Module::Release::Registry
-
-Module::Release::SourceForge
-
-Module::Release::UsePerl
-
-Module::Release::WebUpload::Mojo
 
 Module::Starter::AddModule
 
-Modulino::Base
-
 Modulino::Demo
-
-Modulino::Demo2
-
-Modulino::Demo3
-
-Modulino::Test
-
-Modulino::TestWithBase
-
-MyCPAN::App::BackPAN::Indexer
 
 MyCPAN::App::DPAN
 
-MyCPAN::App::DPAN::Indexer
-
-MyCPAN::App::DPAN::Reporter::AsYAML
-
-MyCPAN::App::DPAN::Reporter::Minimal
-
-MyCPAN::App::DPAN::SkipQueue
-
-MyCPAN::App::Indexer::DPAN
-
 MyCPAN::Indexer
-
-MyCPAN::Indexer::Component
-
-MyCPAN::Indexer::Coordinator
-
-MyCPAN::Indexer::DPAN
-
-MyCPAN::Indexer::Dispatch::Parallel
-
-MyCPAN::Indexer::Dispatch::Serial
-
-MyCPAN::Indexer::Dispatcher::Parallel
-
-MyCPAN::Indexer::Dispatcher::Serial
-
-MyCPAN::Indexer::Interface::ANSIText
-
-MyCPAN::Indexer::Interface::Curses
-
-MyCPAN::Indexer::Interface::Text
-
-MyCPAN::Indexer::Interface::Tk
-
-MyCPAN::Indexer::Notes
-
-MyCPAN::Indexer::NullTester
-
-MyCPAN::Indexer::NullTester::Dispatcher
-
-MyCPAN::Indexer::Queue
-
-MyCPAN::Indexer::Reporter::AsYAML
-
-MyCPAN::Indexer::Reporter::Base
-
-MyCPAN::Indexer::TestCensus
-
-MyCPAN::Indexer::Tutorial
-
-MyCPAN::Indexer::Worker
 
 Net::MAC::Vendor
 
+Net::SSH::Perl::ProxiedIPC
+
+Net::SSH::Perl::WithSocks
+
 Netscape::Bookmarks
-
-Netscape::Bookmarks::Alias
-
-Netscape::Bookmarks::Category
-
-Netscape::Bookmarks::Link
-
-Netscape::Bookmarks::Separator
 
 Object::Iterate
 
-Object::Iterate::Tester
+PeGS::PDF
 
-PPI::App::ppi_version::BDFOY
+Perl::MinimumVersion::Fast
 
-PPT::Util
+Perl::Version
 
-PerlBench::Utils
+perlbench
 
 PerlPowerTools
 
@@ -280,57 +148,35 @@ Pod::InDesign::TaggedText
 
 Pod::InDesign::TaggedText::TPR
 
-Pod::LaTeX::TPR
-
-Pod::Perldoc
-
-Pod::Perldoc::BaseTo
-
-Pod::Perldoc::GetOptsOO
-
-Pod::Perldoc::ToChecker
-
-Pod::Perldoc::ToMan
-
-Pod::Perldoc::ToNroff
-
-Pod::Perldoc::ToPod
-
-Pod::Perldoc::ToRtf
-
-Pod::Perldoc::ToText
-
-Pod::Perldoc::ToTk
-
 Pod::Perldoc::ToToc
 
-Pod::Perldoc::ToXml
+Pod::PseudoPod::DocBook
 
-Pod::SpeakIT::MacSpeech
+Pod::PseudoPod::PerlTricks
 
 Pod::SpeakIt::MacSpeech
 
-Pod::TOC
-
 Pod::WordML
-
-Pod::WordML::AddisonWesley
 
 Polyglot
 
-Polyglot::Tart
+PPI::App::ppi_version::BDFOY
 
-Polyglot::iTunes
+Psychic::Ninja
+
+ReturnValue
 
 Roman::Unicode
+
+scriptdist
 
 Set::CrossProduct
 
 SourceCode::LineCounter::Perl
 
-Surveyor::App
+String::Sprintf
 
-Surveyor::Benchmark::GetDirectoryListing
+Surveyor::App
 
 Surveyor::Benchmark::HTMLEntities
 
@@ -338,15 +184,9 @@ Surveyor::Benchmark::SchwartzianTransform::SortFilesBySize
 
 Task::MasteringPerl
 
+Task::MojoLearningEnvironment
+
 Test::Data
-
-Test::Data::Array
-
-Test::Data::Function
-
-Test::Data::Hash
-
-Test::Data::Scalar
 
 Test::Env
 
@@ -360,27 +200,29 @@ Test::Manifest
 
 Test::Output
 
-Test::Output::Tie
-
 Test::Prereq
 
-Test::Prereq::Build
-
 Test::URI
+
+Test::WWW::Accessibility
 
 Tie::BoundedInteger
 
 Tie::Cycle
 
+Tie::StringArray
+
 Tie::Timely
 
 Tie::Toggle
+
+Unicode::Casing
 
 Unicode::Support
 
 Unicode::Tussle
 
-
+WordPress::Grep
 
 =head1 AUTHOR
 
@@ -388,7 +230,7 @@ brian d foy, C<< <bdfoy@cpan.org> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2006-2015, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2006-2016, brian d foy <bdfoy@cpan.org>. All rights reserved.
 
 You can use this module under the same terms as Perl itself.
 
