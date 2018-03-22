fc49a65a028881075cd3e38e063c9ff95bcaec9c
brian d foy <brian.d.foy@gmail.com>
2018-03-22 14:07:20 -0400

    bump date

--- lib/Task/BDFOY.pm
+++ lib/Task/BDFOY.pm
@@ -243,7 +243,7 @@ brian d foy C<< <bdfoy@gmail.com> >>
 
 =head1 COPYRIGHT AND LICENCE
 
-Copyright © 2014-2017, brian d foy.
+Copyright © 2014-2018, brian d foy.
 
 This code is licensed under the Artisitic License 2.0
 
